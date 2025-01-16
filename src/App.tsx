265ac6c0ca97029675cfa8734b78b8506512951d
Brayan Yepes <abrayan.claye@gmail.com>
2025-01-16 10:16:28 -0500

    feat: add structure base of web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
- return <div>Hola mundo</div>
+  return (
+    <main className="w-full max-h-screen h-screen p-5">
+      <h1>Contact List</h1>
+      <p>Form</p>
+      <p>User List</p>
+      <p>User Details</p>
+    </main>
+  );
 }
 
-export default App
+export default App;
